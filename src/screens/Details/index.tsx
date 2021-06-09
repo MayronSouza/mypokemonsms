cedce81ccf97c3d1f3cfc8f4af8c332a93bcd1c0
MayronSouza <mayron.souza@gmz.solutions>
2021-06-09 17:59:54 -0300

    Finalizando a Details screen

--- src/screens/Details/index.tsx
+++ src/screens/Details/index.tsx
@@ -1,13 +1,23 @@
 import React, { useEffect, useState } from 'react';
-import { SvgUri } from 'react-native-svg';
-
+import {
+  ActivityIndicator,
+  Text,
+  View,
+} from 'react-native';
 
 import { PokeAPI } from '../../assets/api';
 
 import {
   Container,
+  PokeCard,
+  PokeCardTitle,
+  PokeCardDivider,
+  PokeImage,
+  Description,
+  DescriptionText,
+  DescriptionItemText,
+  Loading,
 } from './styles';
-import { ActivityIndicator } from 'react-native';
 
 interface DetailsProps {
   route: {
@@ -17,26 +27,70 @@ interface DetailsProps {
   };
 }
 
+interface AbilitiesProps {
+  ability: {
+    name: string;
+    url: string;
+  }
+}
+
 const Details = ({ route }: DetailsProps) => {
   const { name } = route.params;
+
+  const [abilities, setAbilities] = useState<AbilitiesProps[]>([]);
+  const [experience, setExperience] = useState('');
   const [figure, setFigure] = useState('');
+  const [height, setHeight] = useState('');
+  const [weight, setWeight] = useState('');
 
   useEffect(()=> {
     PokeAPI.get(`/pokemon/${name}`)
-      .then(response => setFigure(response.data.sprites.other.dream_world.front_default))
+      .then(response => {
+        setFigure(response.data.sprites.other.dream_world.front_default);
+        setAbilities(response.data.abilities);
+        setExperience(response.data.base_experience.toString()  + ' exp');
+        setHeight(response.data.height.toString() + ' m');
+        setWeight(response.data.weight.toString() + ' Kg');
+      })
       .catch(error => console.log(error));
   }, [name]);
 
   return (
     <Container>
       {figure !== '' ?
-        <SvgUri
-          width="50%"
-          height="50%"
-          uri={figure}
-          onPress={() => console.log(name)}
-        /> :
-        <ActivityIndicator size="large" color="#080c13" />
+        <PokeCard>
+          <PokeCardTitle>{name.toUpperCase()}</PokeCardTitle>
+          <PokeCardDivider />
+          <PokeImage
+            uri={figure}
+            onPress={() => console.log(name)}
+          />
+          <Description>
+            <DescriptionText>
+              Experiência: {''}
+              <DescriptionItemText>
+                {experience}
+              </DescriptionItemText>
+            </DescriptionText>
+            <DescriptionText>
+              Altura: {''}
+              <DescriptionItemText>
+              {height}
+              </DescriptionItemText>
+            </DescriptionText>
+            <DescriptionText>
+              Peso: {''}
+              <DescriptionItemText>
+              {weight}
+              </DescriptionItemText>
+            </DescriptionText>
+            <DescriptionText>Habilidades:</DescriptionText>
+            {abilities.map(ability => (
+              <DescriptionItemText key={ability.ability.name}>{ability.ability.name}</DescriptionItemText>   
+            ))}
+          </Description>
+        </PokeCard> :
+        <Loading />
       }   
     </Container>
   );
--- src/screens/Details/styles.ts
+++ src/screens/Details/styles.ts
@@ -1,10 +1,12 @@
 import styled from 'styled-components/native';
+import { SvgUri } from 'react-native-svg';
+import { ActivityIndicator } from 'react-native';
 
 const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: #b1a7a7;
+  background-color: #2b2113;
 `;
 
 const Title = styled.Text`
@@ -12,4 +14,80 @@ const Title = styled.Text`
   font-weight: bold;
 `;
 
-export { Container, Title };
+const PokeCard = styled.View`
+  width: 80%;
+  height: 90%;
+  background-color: #ecd1ab;
+  border-bottom-color: #f99000;
+  border-left-color: #f99000;
+  border-top-color: #f99000;
+  border-right-color: #f99000;
+  border-bottom-width: 15px;
+  border-left-width: 15px;
+  border-top-width: 15px;
+  border-right-width: 15px;
+`;
+
+const PokeCardTitle = styled.Text`
+  align-self: center;
+  margin-top: 4px;
+  font-size: 24px;
+  font-weight: bold;
+  color: #2b2113;
+`;
+
+const PokeCardDivider = styled.View`
+  padding: 5px;
+  margin: 5px;
+  border-bottom-color: #f99000;
+  border-bottom-width: 3px;
+`;
+
+const PokeImage = styled(SvgUri).attrs({
+  width: '45%',
+  height: '25%',
+  alignSelf: 'center',
+})``;
+
+const Loading = styled(ActivityIndicator).attrs({
+  size: "large",
+  color:"#f99000",
+})``;
+
+const Description = styled.View`
+  justify-content: space-around;
+  /* align-items: center; */
+  width: 90%;
+  height: 40%;
+  align-self: center;
+  margin-top: 16px;
+`;
+
+const DescriptionText = styled.Text`
+  margin-top: 10px;
+  margin-left: 10px;
+  font-size: 24px;
+  font-weight: bold;
+  color: #2b2113;
+`;
+
+const DescriptionItemText = styled.Text`
+  margin-top: 5px;
+  margin-left: 24px;
+  padding-top: 5px;
+  font-size: 18px;
+  color: #7c4f11;
+`;
+
+export {
+  Container,
+  Title,
+  PokeCard,
+  PokeCardTitle,
+  PokeCardDivider,
+  PokeImage,
+  Loading,
+  Description,
+  DescriptionText,
+  DescriptionItemText,
+};
